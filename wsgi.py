20250b50c0c1cfcff730f825d8578c53a0f8ba9f
Sri-1227 <srinivasmekala1227@gmail.com>
2023-07-03 08:20:11 +0530

    bug fix

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,6 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from application import create_app
-from application import socketio
 
 
 app = create_app()
